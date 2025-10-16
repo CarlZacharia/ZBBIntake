d9e743ab8df2b4c3adb3316f5da287c2b5154455
CarlZacharia <czacharia@pittsburghelderlaw.com>
2025-10-16 18:59:59 -0400

    10162025-1

--- src/app/private/elderlaw/medicaidintake/medicaidintake.component.ts
+++ src/app/private/elderlaw/medicaidintake/medicaidintake.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MedicaidStepsComponent } from '../medicaidsteps/medicaidsteps.component';
 
 @Component({
   selector: 'app-medicaidintake',
-  imports: [CommonModule, MedicaidStepsComponent],
+  imports: [CommonModule, RouterModule, MedicaidStepsComponent],
   templateUrl: './medicaidintake.component.html',
   styleUrl: './medicaidintake.component.css'
 })
 export class MedicaidintakeComponent {
-
+  activesection: any = '';
 }
