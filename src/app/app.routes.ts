247c34b98d02eb568a11e7c9842be42f203df184
Carl B. Zacharia <czacharia@pittsburghelderlaw.com>
2025-11-19 02:43:25 -0500

    Reomved all Facility info 11192025-1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,7 @@ import { Routes } from '@angular/router';
 import { MainPublicComponent } from './main-public/main-public.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { IntakehubComponent } from './private/intakehub/intakehub.component';
-import { MainintakeComponent } from './private/mainintake/mainintake.component';
 import { MedicaidintakeComponent } from './private/elderlaw/medicaidintake/medicaidintake.component';
-import { FacilityhomeComponent } from './private/facilityhome/facilityhome.component';
-import { FacilityProfileComponent } from './private/facilityhome/facilityprofile/facilityprofile.component';
-import { ReferralsharedComponent } from './private/facilityhome/referralshared/referralshared.component';
-import { ProfileDashboardComponent } from './profile/pages/profile-dashboard.component';
 import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -26,41 +20,7 @@ export const routes: Routes = [
     component: RegistrationComponent,
     canActivate: [guestGuard]
   },
-  {
-    path: 'profile',
-    component: ProfileDashboardComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'intakehub',
-    component: IntakehubComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'mainintake',
-    component: MainintakeComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'medicaidintake',
-    component: MedicaidintakeComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'facilityhome',
-    component: FacilityhomeComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'facilityprofile',
-    component: FacilityProfileComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'referral',
-    component: ReferralsharedComponent,
-    canActivate: [authGuard]
-  },
+
   {
     path: '**',
     redirectTo: ''
